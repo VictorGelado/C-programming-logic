38002aadde3d1fb146a449a89594cb9f12992a7a
VictorGelado <VictorGelado>
2024-04-11 13:05:21 -0300

    list 2

--- lista-2/atividade_9.c
+++ lista-2/atividade_9.c
@@ -1,61 +1,65 @@
 #include <stdio.h>
 
 int main(void) {
-
-  int numero, a1, a2, a3, a4;
-  int milhar, dezena, unidade, centena, numAuxiliar;
-
-  while (scanf("%d", &numero) > 9999) {
-    printf("Numero invalido!");
-    scanf("%d", &numero);
-  }
-
-  // milhar
-  numAuxiliar = numero;
-
-  if (numAuxiliar >= 1000) {
-    while (numAuxiliar >= 1000) {
-      numAuxiliar -= 1000;
-    }
-
-    centena = numAuxiliar;
-    milhar = (numero - centena) / 1000;
-  }  else {
-    milhar = 0;
-    centena = numero;
-  }
-
-
-  // dezena
-  numAuxiliar = centena;
-
-  if (numAuxiliar >= 100 || numero >= 1000) {
-    while (numAuxiliar >= 100) {
-      numAuxiliar -= 100;
-    }
-
-    dezena = numAuxiliar;
-    centena = (centena - dezena) / 100;
-  } else {
-    centena = 0;
-    
-    dezena = (numero >= 10)? numero: 0;
-  }
-
-  //unidade
-  numAuxiliar = dezena;
-
-  if (numAuxiliar >= 10 || numero >= 1000 || numero >= 100) {
-    while (numAuxiliar >= 10) {
-      numAuxiliar -= 10;
-    }
-
-    unidade = numAuxiliar;
-    dezena = (dezena - unidade) / 10;
-  } else unidade = numero;
-
-  printf("milhar: %d, centena: %d, dezena: %d, unidade: %d\n\n", milhar, centena, dezena, unidade);
+    int numero;
   
+    scanf("%d", &numero);
+    if (numero > 9999 || numero <= 0) {
+        printf("Numero invalido!");
+        return 0;
+    }
 
-  return 0;
+    int milhar = numero / 1000;
+    int resto = numero % 1000;
+    int centena = resto / 100;
+    resto %= 100;
+    int dezena = resto / 10;
+    int unidade = resto % 10;
+
+    char *ordem;
+    if (numero > 999)
+        ordem = "quarta ordem";
+    else if (numero > 99)
+        ordem = "terceira ordem";
+    else if (numero > 9)
+        ordem = "segunda ordem";
+    else
+        ordem = "primeira ordem";
+
+    char *milharS = (milhar == 1) ? "unidade de milhar" : "unidades de milhar";
+    char *centenaS = (centena == 1) ? "centena" : "centenas";
+    char *dezenaS = (dezena == 1) ? "dezena" : "dezenas";
+    char *unidadeS = (unidade == 1) ? "unidade" : "unidades";
+    
+    printf("(%s) %d = ", ordem, numero);
+    
+    if (milhar > 0)
+        printf("%d %s", milhar, milharS);
+
+    if (centena > 0)
+        printf("%s%d %s", (milhar > 0) ? " + " : "", centena, centenaS);
+    
+    if (dezena > 0)
+        printf("%s%d %s", (milhar > 0 || centena > 0) ? " + " : "", dezena, dezenaS);
+    
+    if (unidade > 0)
+        printf("%s%d %s", (milhar > 0 || centena > 0 || dezena > 0) ? " + " : "", unidade, unidadeS);
+    
+    printf(" = ");
+
+    if (milhar > 0)
+        printf("%d", milhar * 1000);
+
+    if (centena > 0)
+        printf("%s%d", (milhar > 0) ? " + " : "", centena * 100);
+
+    if (dezena > 0)
+        printf("%s%d", (milhar > 0 || centena > 0) ? " + " : "", dezena * 10);
+
+    if (unidade > 0)
+        printf("%s%d", (milhar > 0 || centena > 0 || dezena > 0) ? " + " : "", unidade);
+    
+    printf("\n");
+
+    return 0;
 }
